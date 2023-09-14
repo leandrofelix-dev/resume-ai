22f2c93b60955776f72d71052740ab1a32c8ed07
lehinfo-felix <lehinfo.felix@gmail.com>
2023-09-14 03:03:25 -0300

    first commit

--- src/components/organisms/header.tsx
+++ src/components/organisms/header.tsx
@@ -6,10 +6,10 @@ import { Github } from 'lucide-react'
 export function Header() {
   return (
     <header className="px-6 py-3 flex items-center justify-between border-b">
-    <h1 className="text-xl font-bold">upload.ai</h1>
+    <h1 className="text-xl font-bold">resume.ai</h1>
     <div className="flex items-center gap-3">
       <span className="text-sm text-zinc-muted-foreground">Desenvolvido com amor por Leandro Felix 💜</span>
-      <Separator orientation="vertical" className="h-6"/>
+      <Separator orientation="vertical" className="h-6"/> 
       <Button variant="outline">
         <Github className="w-4 h-4 mr-2"/>
         Github
