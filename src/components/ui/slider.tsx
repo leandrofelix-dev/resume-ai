2b35654b3132bd2da18066e8c719d722dd03fe28
leandrofelix-dev <lehinfo.felix@gmail.com>
2023-12-31 12:11:00 -0300

    feat: alterando cor do slider

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -15,7 +15,7 @@ const Slider = React.forwardRef<
     )}
     {...props}
   >
-    <SliderPrimitive.Track className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-input/60">
+    <SliderPrimitive.Track className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-input/80">
       <SliderPrimitive.Range className="absolute h-full bg-primary" />
     </SliderPrimitive.Track>
     <SliderPrimitive.Thumb className="block h-4 w-4 rounded-full bg-primary shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
